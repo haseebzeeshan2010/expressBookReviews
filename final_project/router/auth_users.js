da79d1bfb0673e70608d9980881c0574f34ee71d
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-20 18:26:34 -0400

    final changes

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,27 +6,23 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-  //write code to check is the username is valid
-  let userswithsamename = users.filter((user)=>{
-    return user.username === username
-  });
-  if(userswithsamename.length > 0){
-    return true;
-  } else {
-    return false;
-  }
+  for (var user in users) {
+    if (user["username"]===username) {
+        return true;
+    }
+}
+  return false;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-  //write code to check if username and password match the one we have in records.
-  let validusers = users.filter((user)=>{
-    return (user.username === username && user.password === password)
-  });
-  if(validusers.length > 0){
-    return true;
-  } else {
-    return false;
-  }
+    let validUser = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+      });
+      if(validUser.length > 0){
+        return true;
+      } else {
+        return false;
+      }
 }
 
 //only registered users can login
@@ -34,59 +30,38 @@ regd_users.post("/login", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  if (!username || !password) {
-      return res.status(404).json({message: "Error logging in"});
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({data: password}, 'access', {expiresIn: 60*60});
+    req.session.authorization = {accessToken, username}
+    return res.status(200).json({message: "User successfully logged in."})
   }
-
-  if (authenticatedUser(username,password)) {
-    let accessToken = jwt.sign({
-      data: password
-    }, 'access', { expiresIn: 60 * 60 });
-
-    req.session.authorization = {
-      accessToken,username
-    }
-  return res.status(200).send("User successfully logged in");
-  } else {
-    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  else {
+    return res.status(300).json({message: "Username and password do not match our records. Please try again."});
   }
 });
 
 // Add a book review
-regd_users.use("/review/", function auth(req,res,next){
-  if(req.session.authorization) {
-      token = req.session.authorization['accessToken'];
-      jwt.verify(token, "access",(err,user)=>{
-          if(!err){
-              req.user = req.session.authorization['username'];
-              next();
-          }
-          else{
-              return res.status(403).json({message: "User not authenticated"})
-          }
-       });
-   } else {
-       return res.status(403).json({message: "User not logged in"})
-   }
-});
-
-regd_users.put("/review/add/:isbn",function (req,res){
-  const isbn = req.params.isbn;
-  const user = req.user
-  const comment = req.query.review
-  const review = { 
-      [user] : comment
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  let isbn = parseInt(req.params.isbn);
+  if (books[isbn]) {
+      books[isbn].reviews[req.session.authorization.username] = req.body.review;
+      return res.status(200).json({message: "Review successfully posted."});
   }
-
-  books[isbn].reviews = review
-  res.send(JSON.stringify(books[isbn].reviews,null,4));
+  else {
+      return res.status(300).json({message: "ISBN not found."});
+  }
 });
 
-regd_users.delete("/review/:isbn", (req, res) => {
-  const isbn = req.params.isbn;
-  const user = req.user
-  delete books[isbn].reviews[user]
-  res.send(books[isbn])
+//delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  let isbn = parseInt(req.params.isbn);
+  if (books[isbn]) {
+      delete books[isbn].reviews[req.session.authorization.username];
+      return res.status(200).json({message: "Review successfully deleted."});
+  }
+  else {
+      return res.status(300).json({message: "ISBN not found."});
+  }
 });
 
 module.exports.authenticated = regd_users;
